89909db241236bb496dd6f9cabc011e2a0eb44d0
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2025-12-22 17:17:45 -0300

    Update auth.service.js

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import api from "@/services/api";
 
 export async function login(email, senha) {
-  const { data } = await api.post("/login", {
+  const { data } = await api.post("/login/login", {
     usu_email: email,
     usu_senha: senha,
   });
