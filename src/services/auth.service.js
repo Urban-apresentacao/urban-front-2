2f95c5c720eca8c8e191e8f0234b7d52906ea5f4
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2025-12-23 13:22:38 -0300

    correção do path da rota de login

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import api from "@/services/api";
 
 export async function login(email, senha) {
-  const { data } = await api.post("/login/login", {
+  const { data } = await api.post("/login", {
     usu_email: email,
     usu_senha: senha,
   });
